9cf8a65cb4729bd2346e99644e10dc227bb0776e
bitsf <zzm8415@163.com>
2024-07-04 16:47:00 +0800

    refactor decoder handler
    Signed-off-by: bitsf <zzm8415@163.com>

--- pkg/handler.go
+++ pkg/handler.go
@@ -11,3 +11,24 @@ type BaseHandler struct {
 func (BaseHandler) NeedInput() bool {
 	return true
 }
+
+type IEncodeDecodeHandler interface {
+	Encode(input string) (output string, err error)
+	Decode(input string) (output string, err error)
+}
+
+type EncodeDecodeHandler struct {
+	BaseHandler
+	IEncodeDecodeHandler
+}
+
+func (h EncodeDecodeHandler) Handle(input string, params []string, current_ind int) (output string, inc_i int, err error) {
+	if current_ind+1 < len(params) {
+		if params[current_ind+1] == "-decode" {
+			output, err = h.Decode(input)
+			return output, 1, nil
+		}
+	}
+	output, err = h.Encode(input)
+	return output, 0, nil
+}
--- pkg/handlers/base64_handler.go
+++ pkg/handlers/base64_handler.go
@@ -6,24 +6,30 @@ import (
 )
 
 type Base64Handler struct {
-	pkg.BaseHandler
+	pkg.EncodeDecodeHandler
 }
 
-func (s Base64Handler) Handle(input string, params []string, current_ind int) (output string, inc_i int, err error) {
-	if current_ind+1 < len(params) {
-		if params[current_ind+1] == "-decode" {
-			ss, _err := (base64.StdEncoding.DecodeString(input))
-			if _err != nil {
-				err = _err
-				return
-			}
-			return string(ss), 1, nil
-		}
-	}
+func (s Base64Handler) Encode(input string) (output string, err error) {
 	output = base64.StdEncoding.EncodeToString([]byte(input))
 	return
 }
 
+func (s Base64Handler) Decode(input string) (output string, err error) {
+	ss, _err := (base64.StdEncoding.DecodeString(input))
+	if _err != nil {
+		err = _err
+		return
+	}
+
+	return string(ss), nil
+}
+
+func NewBase64Handler() Base64Handler {
+	h := Base64Handler{pkg.EncodeDecodeHandler{}}
+	h.IEncodeDecodeHandler = h
+	return h
+}
+
 func init() {
-	pkg.Register("base64", &Base64Handler{})
+	pkg.Register("base64", NewBase64Handler())
 }
